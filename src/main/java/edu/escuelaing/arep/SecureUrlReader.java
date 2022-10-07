a963483ed3fc578b281de916d5703a7b6b693bc1
unknown <diego.leon-ba@labinfo.is.escuelaing.edu.co>
2022-10-07 13:51:56 -0500

    Se modifican las clases

--- src/main/java/edu/escuelaing/arep/SecureUrlReader.java
+++ src/main/java/edu/escuelaing/arep/SecureUrlReader.java
@@ -17,13 +17,12 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
 public class SecureUrlReader {
-
-    public static void main(String[] args) {
+    public static void loadTrustStore(String path, String passwd) {
         try {
 
             // Create a file and a password representation
-            File trustStoreFile = new File("keystores/ecikeystore.p12");
-            char[] trustStorePassword = "arep123".toCharArray();
+            File trustStoreFile = new File(path);
+            char[] trustStorePassword = passwd.toCharArray();
 
             // Load the trust store, the default type is "pkcs12", the alternative is "jks"
             KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
@@ -46,14 +45,7 @@ public class SecureUrlReader {
             SSLContext sslContext = SSLContext.getInstance("TLS");
             sslContext.init(null, tmf.getTrustManagers(), null);
             SSLContext.setDefault(sslContext);
-
-            // We can now read this URL
-            readURL("https://localhost:5000/hello");
-
-            // This one can't be read because the Java default truststore has been 
-            // changed.
-            readURL("https://www.google.com");         
-        
+            
         } catch (KeyStoreException ex) {
             Logger.getLogger(SecureUrlReader.class.getName()).log(Level.SEVERE, null, ex);
         } catch (FileNotFoundException ex) {
@@ -70,7 +62,8 @@ public class SecureUrlReader {
 
     }
 
-    public static void readURL(String sitetoread) {
+    public static String readURL(String sitetoread) {
+        String res="";
         try {
             // Crea el objeto que representa una URL2
             URL siteURL = new URL(sitetoread);
@@ -102,9 +95,11 @@ public class SecureUrlReader {
             String inputLine = null;
             while ((inputLine = reader.readLine()) != null) {
                 System.out.println(inputLine);
+                res += inputLine;
             }
         } catch (IOException x) {
             System.err.println(x);
         }
+        return res;
     }
 }
--- src/main/java/edu/escuelaing/arep/SparkAppTwo.java
+++ src/main/java/edu/escuelaing/arep/SparkAppTwo.java
@@ -2,24 +2,24 @@ package edu.escuelaing.arep;
 
 import static spark.Spark.*;
 
-public class SparkApp{
+public class SparkAppTwo{
     public static void main(String[] args) {
         port(getPort());
         secure(getKeyStore(), getPasswd(), null, null); 
-        SecureUrlReader.loadTrustStore(SparkAppTwo.getKeyStore(), SparkAppTwo.getPasswd());
-        get("/hello", (req, res) -> "Hello SparkApp 1");
-        get("/remote", (req, res) -> SecureUrlReader.readURL(SparkAppTwo.getUrl()));
+        SecureUrlReader.loadTrustStore(SparkApp.getKeyStore(), SparkApp.getPasswd());
+        get("/hello", (req, res) -> "Hello SparkApp 2");
+        get("/remote", (req, res) -> SecureUrlReader.readURL(SparkApp.getUrl()));
     }
 
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+        return 5001; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 
     public static String getKeyStore(){
-        return "keystores/ecikeystore.p12";
+        return "keystores/ecikeystore2.p12";
     }
 
     public static String getPasswd(){
@@ -27,7 +27,7 @@ public class SparkApp{
     }
 
     public static String getUrl(){
-        return "https://localhost:5000/hello";
+        return "https://localhost:5001/hello";
     }
 
 } 
